15817604d886b486184cd3b9d68783df00a77649
MaazB <maazb@bitbucket.com>
2025-05-18 15:25:10 +0500

    Clock button added and functionalized in create task

--- app/src/main/java/com/example/practicethree/CreateTaskActivity.kt
+++ app/src/main/java/com/example/practicethree/CreateTaskActivity.kt
@@ -5,25 +5,49 @@ import android.view.MotionEvent
 import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.cardview.widget.CardView
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.text.SimpleDateFormat
+import java.util.Date
+import java.util.Locale
 
 class CreateTaskActivity : AppCompatActivity() {
+
+    private var selectedDate : Date = Date()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_create_task)
 
+        val dateTv = findViewById<TextView>(R.id.addTaskDateTV)
+        setCurrentDayOnInit(dateTv)
+
         val createTaskCross = findViewById<ImageView>(R.id.imageViewCross)
         crossButtonOnClick(createTaskCross)
+
+        val createTaskClock = findViewById<CardView>(R.id.addTaskClockCV)
+        createTaskClock.setOnClickListener{
+            selectDate(dateTv)
+        }
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    fun setCurrentDayOnInit(bigTV : TextView){
+        val currentDate = Date()
+        val formatter = SimpleDateFormat("dd MMM yyyy", Locale.getDefault())
+        val todayDay : String = formatter.format(currentDate)
+        bigTV.text = todayDay
     }
 
     override fun dispatchTouchEvent(ev: MotionEvent?): Boolean {
@@ -47,6 +71,20 @@ class CreateTaskActivity : AppCompatActivity() {
         }
 
         return super.dispatchTouchEvent(ev)
+    }
+
+    fun selectDate(dateTV : TextView){
+        val dateInMS = selectedDate.time
+        val datePickerBuilder = MaterialDatePicker.Builder.datePicker().setSelection(dateInMS).setTitleText("Select Date")
+        val datePicker = datePickerBuilder.build()
+        datePicker.addOnPositiveButtonClickListener { selection ->
+            selectedDate = Date(selection)
+            val formatter = SimpleDateFormat("dd MMM yyyy", Locale.getDefault())
+            val todayDay : String = formatter.format(selectedDate)
+            dateTV.text = todayDay
+        }
+        datePicker.show(supportFragmentManager, "Date Picker")
+
     }
 
     fun crossButtonOnClick(imageView : ImageView){
